099e94f60e3c4850260013e84ebed5e0666045ae
gain Kim <gain515253@naver.com>
2023-04-05 10:49:24 +0900

    commit0330

---
+++ src/main/java/com/example/demo/web/dto/HelloResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
+//    public HelloResponseDto(String name, int amount){
+//        this.name=name;
+//        this.amount=amount;
+//    }
+}
